5c38a7270795cc9f42e3ac556d419a191406b489
Toon Willems <m@toonwillems.be>
2019-01-31 18:07:37 +0000

    fix some things

--- presence.sh
+++ presence.sh
@@ -166,17 +166,17 @@ scan () {
 	fi
 }
 
-scan_ip () {
+ip_scan () {
 	if [ ! -z "$1" ]; then
-		local result=$(ping -c 5 "$1" 2>&1 )
+		local result=$(ping -c 5 "$1" 2>&1 | grep 'from' && echo 'present' )
 		debug_echo "Scan result: [$result]"
 		echo "$result"
 	fi
 }
 
-scan_arp () {
+arp_scan () {
 	if [ ! -z "$1" ]; then
-		local result=$(sudo arping -c 5 "$1" 2>&1 | grep 'from' )
+		local result=$(sudo arping -c 5 "$1" 2>&1 | grep 'from' && echo 'present' )
 		debug_echo "Scan result: [$result]"
 		echo "$result"
 	fi
@@ -234,6 +234,10 @@ while true; do
 		#CLEAR PER-LOOP VARIABLES
 		name_scan_result=""
 		name_scan_result_verify=""
+		ip_scan_result=""
+		ip_scan_result_verify=""
+		arp_scan_result=""
+		arp_scan_result_verify=""
 		ok_to_publish=1
 
 		#OBTAIN INDIVIDUAL ADDRESS
@@ -260,7 +264,7 @@ while true; do
 		duration_timer=$(( (end_time - start_timer) / 1000000 ))
 
 		#THIS DEVICE NAME IS PRESENT
-		if [ "$name_scan_result" != "" ]; then
+		if [ "$name_scan_result" != "" || "$ip_scan_result" != "" || "$arp_scan_result" != "" ]; then
 
 			#STATE IS SAME && ONLY REPORT CHANGES THEN DISABLE PUBLICATION
 			[ "${device_statuses[$index]}" == '100' ] && [ "$changes_only" == 1 ] && ok_to_publish=0
